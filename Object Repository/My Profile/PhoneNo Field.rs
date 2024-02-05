ed2710a37254cb301599c500156dfd175f6b2468
joymartinez-ubidy <joy.martinez@ubidy.com>
2024-02-05 10:42:12 +0800

    Add script for My Profile

---
+++ Object Repository/My Profile/PhoneNo Field.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>PhoneNo Field</name>
+   <tag></tag>
+   <elementGuidId>2fead919-201b-4c2e-ad98-14f403d23bc8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id = 'phoneNo']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>phoneNo</value>
+      <webElementGuid>3148c247-bfa0-480a-84af-251fb64b143e</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/My Profile/Save AND Continue btn.rs
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Save AND Continue btn</name>
+   <tag></tag>
+   <elementGuidId>33056ab9-efed-4568-989d-bae1b62781a6</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//button[(text() = 'Save &amp; Continue' or . = 'Save &amp; Continue')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+      <webElementGuid>85d6d070-6fdf-44d2-ae73-8eaf2879f9ab</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Save &amp; Continue</value>
+      <webElementGuid>a3fe9c9e-f6f4-4711-a9a4-6441a37bf087</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
