02cedddaaab432c3c3dc33b0fbd369302c7933e7
joymartinez-ubidy <joy.martinez@ubidy.com>
2024-02-14 10:13:09 +0800

    Add Head addressess

---
+++ Object Repository/My Profile/Textboxes/Address Phone Txtboxes.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Address Phone Txtboxes</name>
+   <tag></tag>
+   <elementGuidId>9c915d41-6a7e-4918-bddf-32962bfb8218</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id = 'phone']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>phone</value>
+      <webElementGuid>4e076ee9-29fd-409d-8732-a695cd5e2624</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/My Profile/Textboxes/City Txtboxes.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>City Txtboxes</name>
+   <tag></tag>
+   <elementGuidId>646411f0-a28e-45ba-bf26-379c4d08dafe</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id = 'city']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>city</value>
+      <webElementGuid>5976a844-f5d7-4f0d-89a0-d5b66958458c</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
