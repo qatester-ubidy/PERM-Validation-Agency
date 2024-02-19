4263bdbbae68fd7f9936902b8923fdb7c3b582c5
JerryMacapagal-UBIDY <jerry.macapagal@ubidy.com>
2024-02-19 18:04:57 +0800

    Verify if user can receive email notification for declined application

---
+++ Object Repository/Gmail Page/Elements/First Email Decline Application Notif.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>First Email Decline Application Notif</name>
+   <tag></tag>
+   <elementGuidId>32ba0050-c828-47d7-8a93-e57df6b52ebe</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>/html/body/div[8]/div[3]/div/div[2]/div[4]/div/div/div/div[2]/div/div[1]/div/div[3]/div/div[2]/div[1]/div[2]/div[6]/div[1]/div/table/tbody/tr[1]/td[6]/div/div/div[2]/span/span/span[text()=&quot;Declined&quot;]/parent::span</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html/body/div[8]/div[3]/div/div[2]/div[4]/div/div/div/div[2]/div/div[1]/div/div[3]/div/div[2]/div[1]/div[2]/div[6]/div[1]/div/table/tbody/tr[1]/td[6]/div/div/div[2]/span/span/span[text()=&quot;Declined&quot;]/parent::span</value>
+      <webElementGuid>8843db3c-ed9e-4ed3-bfe6-4e1c56effcff</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
