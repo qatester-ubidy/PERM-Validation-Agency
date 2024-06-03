52d7f72a251f0cff7a54f5da58ccfea1ac2755f4
JerryMacapagal-UBIDY <jerry.macapagal@ubidy.com>
2024-06-03 10:57:02 +0800

    Enhance click on Email Notification

--- Scripts/Email Notification/TC001 - Verify if user can receive email notification for application approved/Script1708098372895.groovy
+++ Scripts/Email Notification/TC001 - Verify if user can receive email notification for application approved/Script1708098372895.groovy
@@ -32,7 +32,7 @@ WebUI.waitForElementPresent(findTestObject('Object Repository/Gmail Page/Element
 
 WebUI.delay(5)
 
-WebUI.click(findTestObject('Object Repository/Gmail Page/Elements/First Email Approved Engagement Notif'))
+WebUI.enhancedClick(findTestObject('Object Repository/Gmail Page/Elements/First Email Approved Engagement Notif'), FailureHandling.STOP_ON_FAILURE)
 
 WebUI.verifyTextPresent("Application has been approved", false)
 
